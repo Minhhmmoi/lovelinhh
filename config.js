34b6908d6deaddeff1498ceb451b8f94c14cf457
Minhhmmoi <127021373+Minhhmmoi@users.noreply.github.com>
2023-03-06 11:03:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Yêu Khánh Linh",
+    introTitle: 'Hello Linh nhé',
+    introDesc: `Anh có điều này muốn hỏi, em nhớ trả lời thật lòng nha :3`,
+    btnIntro: '^^Dạ^^',
+    title: 'Có Phải Linh yêu anh nhiều lắm phải không nào 🥰',
+    desc: 'Không nói coi như là em rất yêu anh đó nha ',
+    btnYes: 'Yêu ơi là yêu <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu anh không cần lý do. em yêu anh lắm',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều lắm 😘😘',
+    messDesc: 'Chúc em một ngày 8/3 vui vẻ',
+    btnAccept: 'Dạ',
+    messLink: 'https://minhhmmoi.github.io/linhiudau/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
